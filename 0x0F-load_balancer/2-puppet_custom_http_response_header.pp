0c12b7629ebc78e39978b25bd58710641a9a61d3
Mythamor <bethmithamo.bm@gmail.com>
2023-09-06 01:20:23 +0300

    HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,16 @@
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# Create an HTML file with "Hello World!" content
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Define a custom fact to retrieve the hostname
-$custom_hostname = $facts['hostname']
-
-# Configure Nginx to add the custom header
-file { '/etc/nginx/conf.d/custom-header.conf':
-  ensure  => file,
-  content => "add_header X-Served-By $custom_hostname always;",
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/conf.d/custom-header.conf'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
-
-# Apply the Nginx configuration
-exec { 'nginx-config-test':
-  command => '/usr/sbin/nginx -t',
-  refreshonly => true,
-  subscribe   => [
-    File['/etc/nginx/conf.d/custom-header.conf'],
-    Service['nginx'],
-  ],
-}
-
-# Notify the Nginx service restart
-Exec['nginx-config-test'] -> Service['nginx']
