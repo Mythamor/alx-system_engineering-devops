1474b9b8a98155ec5bc650d6f22127e99e9c9e03
Mythamor <bethmithamo.bm@gmail.com>
2023-09-26 00:00:14 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     print(f"Employee {user_name} is done with tasks({done_tasks}\
 /{total_tasks}):")
     for title in completed_tasks:
-        print(f"\t{title}")
+        print(f"\t {title}")
