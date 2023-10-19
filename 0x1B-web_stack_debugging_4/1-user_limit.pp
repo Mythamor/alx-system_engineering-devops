26491a304d8adda57126049f5f328c4f7527e8cf
Mythamor <bethmithamo.bm@gmail.com>
2023-10-19 20:47:02 +0300

    Increasing User Limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,14 @@
-#fix os error change config
-exec { 'change_config':
-  command => "sudo /bin/sed -i 's/nofile 4/nofile 6000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
+# Change the OS configuration so that it is possible to login with the 
+holberton user and open a file without any error message.
+
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
